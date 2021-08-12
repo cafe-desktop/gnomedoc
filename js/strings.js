6244702e3d95bba06bb62936605b17d4fd693fb3
Pablo Barciela <scow@riseup.net>
2021-08-12 02:49:30 +0200

    initial

---
+++ js/strings.js
@@ -0,0 +1,5 @@
+
+var language_cookie_text = "Preferred language is loaded from a cookie.";
+var language_missing_text = "Unable to display document in preferred language loaded from cookie, as translation probably does not exist.";
+var remove_cookie_text = "Remove cookie";
+      
